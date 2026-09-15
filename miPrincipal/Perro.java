923f8b478ba37566f44793a6d209b21c5fd8d04d
BraulioMartinezz <L23171179@culiacan.tecnm.mx>
2026-09-15 15:54:36 +0000

    Agregar clase Perro y actualizar Principal para su uso

---
+++ miPrincipal/Perro.java
@@ -0,0 +1,21 @@
+package miPrincipal;
+public class Perro {
+    //atributos
+    private float peso;
+    private int edad;
+    private String tamano,color,raza,nombre;
+    //constructores
+    public Perro(){
+
+    }
+    public Perro(float p, int e, String t, String c, String r, String n){
+    peso = p;
+    edad = e;
+    tamano = t;
+    color = c;
+    raza = r;
+    nombre = n;
+
+    }
+
+}
--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -7,5 +7,21 @@ public class Principal {
 
     public static void main(String[] args) {
         System.out.println(new Principal().getGreeting());
+
+        Perro perro ; // Declaarando la variable perro de tipo perro
+        perro = new Perro() ;
+
+        /*
+        MARCA ERROR PORQUE LOS ATRIBUTOS SON PRIVADOS
+        POR LO TANTO NO PUEDES SER MODIFICADOS/ACCEDIDOS POR UNA
+        CLASE DIFERENTE A PERRO, EN ESTE CASO LA CLASE PRINCIPAL
+        ESTA TRATANDO DE MODIFICARLO 
+        
+        perro nombre = "mila";
+        perro.color = "cafe" ;
+        perro.peso =  23.5 ; 
+        perro.edad = 12 ;
+        perro.raza = "golden"; 
+        perro.tamano = "chico"*/
     }
 }
